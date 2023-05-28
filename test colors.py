3319682c6dd6ee6326eebc8aee8a0259959fc409
bai <batuhanakkurt000@gmail.com>
2023-05-28 15:36:16 +0300

    .

---
+++ test colors.py
@@ -0,0 +1,21 @@
+from colorama import init, Fore
+import os
+init()
+
+os.system("clear")
+
+print(Fore.RED + "red")
+print(Fore.GREEN + "green")
+print(Fore.CYAN + "cyan")
+print(Fore.BLUE + "blue")
+print(Fore.YELLOW + "yellow")
+print(Fore.WHITE + "white")
+print(Fore.MAGENTA + "magenta")
+
+"red" \
+"green" \
+"blue" \
+"cyan" \
+"white" \
+"magenta" \
+"yellow"
